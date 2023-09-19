4db7e6656a94cbf3f9796eb179ad4774430e0e38
Shannen <swilson.prsvr@gmail.com>
2023-09-19 12:20:48 -0400

    Finished test

---
+++ main.js
@@ -0,0 +1,98 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+let myFirstName = "Shannen"
+
+let myLastName = "Wilson"
+
+let myBestFriend = "Will"
+
+let mySentence = "My name is " + myFirstName + " " + myLastName + " and my bestfriend is my fiance " + myBestFriend
+
+let myFavoriteNum = 666
+
+let myMentalAge = 17
+
+let myHeight = 68
+
+let myFavoriteYear = 2019
+
+let mySum = 3 + 1918 + 24 + 1
+
+let myProduct = 232 * 43 / 2 + 4
+
+let myHeightAndAge = myMentalAge + myHeight
+ 
+
+
+
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myBestFriend === "undefined") {
+  myBestFriend = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myFavoriteNum === "undefined") {
+  myFavoriteNum = undefined;
+}
+
+if (typeof myMentalAge === "undefined") {
+  myMentalAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myBestFriend,
+  mySentence,
+  myFavoriteNum,
+  myMentalAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
